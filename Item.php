3404cbfe4222a6ecdb5bbda48a9574480db415fd
Vluuks <vluuks@gmail.com>
2019-09-16 16:21:26 +0200

    updating item model and db schema

--- Item.php
+++ Item.php
@@ -10,10 +10,12 @@
         protected $on_sale;
         protected $category;
 
-        function __construct($id, $name, $price, $description, $tags, $on_sale, $category) {
+        function __construct($id, $name, $inventory, $price, $description, $tags, $on_sale, $category) {
             $this->id = $id;
             $this->name = $name;
+            $this->inventory = $inventory;
             $this->price = $price;
+
             $this->description = $description;
             $this->tags = $tags ?? ['test', 'test'];
             $this->on_sale = $onsale ?? false;
@@ -28,15 +30,16 @@
             return "€" . $this->price;
         }
 
-
         public function jsonSerialize() {
             return [
                 'id' => $this->id,
+                'inventory', => $this->inventory,
                 'name' => $this->name,
                 'price' => $this->getPriceEuros(),
                 'description' => $this->description,
                 'tags' => $this->tags,
-                'onSale'
+                'onSale' => $this->on_sale,
+                'category' => $this->category
             ];
         }
 
