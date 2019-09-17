dc8825a851fad4f5f49e6b179c6675f90536ad13
Vluuks <vluuks@gmail.com>
2019-09-17 10:29:09 +0200

    add item page start

---
+++ Admin/add_item.php
@@ -0,0 +1,8 @@
+<?php
+
+    require("/Classes/DatabaseHelper.php");
+    require("/Classes/Item.php");
+
+    echo("test");
+
+?>
--- Classes/Item.php
+++ Classes/Item.php
@@ -31,7 +31,6 @@
 
         public function getStringifiedTags() {
             $string = trim(json_encode($this->tags), '"');
-            echo $string;
             return $string;
         }
 
