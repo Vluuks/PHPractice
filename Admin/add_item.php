547e160e25836874299ee2e9fa76c2a6640dc90b
Vluuks <vluuks@gmail.com>
2019-09-17 11:04:36 +0200

    form basics

--- Admin/add_item.php
+++ Admin/add_item.php
@@ -1,8 +1,27 @@
 <?php
 
-    require("/Classes/DatabaseHelper.php");
-    require("/Classes/Item.php");
+    require("../Classes/DatabaseHelper.php");
+    require("../Classes/Item.php");
 
     echo("test");
+
+    echo $_POST["on_sale"];
+
+    $testdb = DatabaseHelper::getInstance();
+
+    $item = new Item(
+        null,
+        $_POST["name"],
+        $_POST["inventory"],
+        $_POST["price"],
+        $_POST["description"],
+        array($_POST["tags"]),
+        $_POST["on_sale"],
+        $_POST["category"],
+        $_POST["image"]
+    );
+
+    $testdb->insert($item);
+    $testdb->selectAll();
 
 ?>
